ac1221389847d49a9956066c9fb68d97e19a0932
jiangxing <jiangxing@live.cn>
2026-09-23 18:32:09 +0800

    feat(app-host): export App Manager ActionHost adapter

--- src/app-host/index.ts
+++ src/app-host/index.ts
@@ -4,3 +4,4 @@ export * from "./memory-source.js";
 export * from "./app-manager-source.js";
 export * from "./browser-shell.js";
 export * from "./action-executor.js";
+export * from "./app-manager-action-host.js";
